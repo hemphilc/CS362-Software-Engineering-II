298baf5fc22799477ea7fd4983196478647771df
Corey Hemphill <hemphilc@oregonstate.edu>
2017-08-19 02:53:50 -0700

    SE2

--- BinarySearchTreeUnitTesting/unittest2.c
+++ BinarySearchTreeUnitTesting/unittest2.c
@@ -4,14 +4,14 @@
 #include "structs.h"
 
 struct Node {
-	TYPE val;
-	struct Node *left;
-	struct Node *right;
+    TYPE val;
+    struct Node *left;
+    struct Node *right;
 };
 
 struct BSTree {
-	struct Node *root;
-	int cnt;
+    struct Node *root;
+    int cnt;
 };
 
 TYPE _leftMost(struct Node *cur);
@@ -22,18 +22,21 @@ void testLeftMost() {
     struct BSTree *tree = buildBSTTree();
     
     struct data myData1;
-	struct data myData2;
-	struct data myData3;
-	struct data myData4;
+    struct data myData2;
+    struct data myData3;
+    struct data myData4;
 		
-	myData1.number = 50;
-	myData1.name = "rooty";
-	myData2.number = 13;
-	myData2.name = "lefty";
-	myData3.number = 110;
-	myData3.name = "righty";
-	myData4.number = 10;
-	myData4.name = "lefty of lefty";
+    myData1.number = 50;
+    myData1.name = "rooty";
+	
+    myData2.number = 13;
+    myData2.name = "lefty";
+	
+    myData3.number = 110;
+    myData3.name = "righty";
+	
+    myData4.number = 10;
+    myData4.name = "lefty of lefty";
     
     if (compare(_leftMost(tree->root), &myData4) == 0)
         printf("_leftMost(): PASS left most of root\n");
